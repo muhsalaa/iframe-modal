2220e3606d25c0988210bf3699ffd7fc3e226c46
muhsalaa <muhsalaa@gmail.com>
2022-05-24 10:30:47 +0700

    test commit without ignored dist

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ function hideFrame(e: MessageEvent) {
   if (e.data === 'hide') {
     const someIframe = window.parent.document.getElementById(IFRAME_ID)!;
     someIframe.parentNode!.removeChild(someIframe);
-    window.removeEventListener('message', hideFrame);
+    window.removeEventListener('messages', hideFrame);
   }
 }
 
